1732f3b1f1030d413abcdf353d4630cb1cdf81ba
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-06-18 14:24:33 +0300

    edited: manage media

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.reciept.urls", namespace="api_reciept")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
