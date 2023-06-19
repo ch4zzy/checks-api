a799211c372a1eca2c960a80084827f67cbbbcb1
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-06-19 16:23:01 +0300

    edited: added routers for RetrieveUpdate

--- apps/reciept/urls.py
+++ apps/reciept/urls.py
@@ -1,7 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from apps.reciept.views import CheckListViewSet, CheckViewSet
+from apps.reciept.views import (
+    CheckListViewSet,
+    CheckViewSet,
+    RetrieveUpdateCheckViewSet,
+)
 
 app_name = "reciept"
 
@@ -9,6 +13,7 @@ router = DefaultRouter()
 
 router.register(r"check", CheckListViewSet, basename="check")
 router.register(r"create", CheckViewSet, basename="create")
+router.register(r"checkfile", RetrieveUpdateCheckViewSet, basename="check-file")
 
 urlpatterns = [
     path("", include(router.urls)),
