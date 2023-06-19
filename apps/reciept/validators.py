600308599eceb5accd6eeca08bdfd0afb45d4df1
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-06-19 16:23:53 +0300

    edited: added validator for file check

--- apps/reciept/validators.py
+++ apps/reciept/validators.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.exceptions import ValidationError
 
 from apps.reciept.models import Check, Printer
@@ -12,3 +13,9 @@ def validate_printers(point_id):
 def validate_order(order_id):
     if Check.objects.filter(order__contains={"order_id": order_id}).exists():
         raise ValidationError("Check with the specified order_id already exists.")
+
+
+def validate_pdf(check_id):
+    check = Check.objects.get(id=check_id)
+    if not check.pdf_file:
+        raise status.HTTP_404_NOT_FOUND
