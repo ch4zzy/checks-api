25f96bba490f4375840163ffaaccbc68c1491cec
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-08-25 17:23:12 +0300

    edited: test for views

--- apps/reciept/tests/conftest.py
+++ apps/reciept/tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from apps.reciept.models import Printer, Check
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 @pytest.fixture
@@ -29,7 +30,34 @@ def check_data(printer_create):
         "check_type": "client",
         "order": {"item": "Test Item"},
         "status": "new",
-        "pdf_file": None
+        "pdf_file": ""
+    }
+
+
+@pytest.fixture
+def check_data_pdf(printer_create):
+    pdf_content = b"PDF content"
+    uploaded_pdf = SimpleUploadedFile("test.pdf", pdf_content, content_type="application/pdf")
+
+    return {
+        "printer": printer_create,
+        "check_type": "client",
+        "order": {"item": "Test Item"},
+        "status": "new",
+        "pdf_file": uploaded_pdf
+    }
+
+
+@pytest.fixture
+def check_order_data():
+    return {
+        "point_id": 1,
+        "order": {
+            "order_id": 1,
+            "item": "Milk",
+            "quantity": 2,
+            "price": 10.99
+        }
     }
 
 
