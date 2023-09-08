492bf23849a047dfc2d0e7e7ec9ede9e8265bc0e
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-09-08 23:14:07 +0300

    edited: hotfix filename

--- apps/reciept/dynamic_preferences_registry.py
+++ apps/reciept/dynamic_preferences_registry.py
@@ -1,6 +1,6 @@
 from dynamic_preferences.preferences import Section
-from dynamic_preferences.types import StringPreference
 from dynamic_preferences.registries import global_preferences_registry
+from dynamic_preferences.types import StringPreference
 
 company = Section("company")
 
